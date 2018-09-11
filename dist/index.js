ebc14aa7305ac9e0ff0c1db3112026b869c083e2
Andrey Stepanov <andreystepanov.blog@gmail.com>
2018-09-11 23:42:31 +0700

    Forgot to run a build process :)

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = requestId;
 
+var _debug = _interopRequireDefault(require("debug"));
+
 var _en = _interopRequireDefault(require("nanoid-good/locale/en"));
 
 var _generate = _interopRequireDefault(require("nanoid-good/generate"));
@@ -12,9 +14,11 @@ var _generate = _interopRequireDefault(require("nanoid-good/generate"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const nanoid = (0, _generate.default)(_en.default);
+const logger = (0, _debug.default)('koa:nano-request-id');
 
 function requestId(options) {
   const defaults = {
+    key: 'X-Request-Id',
     prefix: '',
     sufix: '',
     length: 21,
@@ -28,7 +32,7 @@ function requestId(options) {
       length,
       alphabet,
       generate,
-      header = 'X-Request-Id'
+      key
     } = defaults;
     const isCustom = generate && typeof generate === 'function';
     const generatedId = isCustom ? generate() : nanoid(alphabet, parseInt(length, 10));
@@ -36,7 +40,8 @@ function requestId(options) {
     if (generatedId) {
       const id = `${prefix}${generatedId}${sufix}`;
       ctx.request.id = id;
-      ctx.set(header, id);
+      ctx.set(key, id);
+      logger('%s: %s', key, id);
     }
 
     return next();
